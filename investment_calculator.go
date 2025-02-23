73dc5e1228abe7f347406c88ea04dd3fa1af1e47
zAjayi <zemirajayi2805@gmail.com>
2025-02-23 18:03:25 +0100

    my message

--- investment_calculator.go
+++ investment_calculator.go
@@ -45,8 +45,9 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func futureValueCalculator(investmentAmount float64, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
-	rfv := fv / math.Pow(1 + inflationRate/100, years)
+func futureValueCalculator(investmentAmount float64, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1 + inflationRate/100, years)
 	return fv, rfv
+	//return
 }
